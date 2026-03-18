4f382fa8bbde87bf73199b9e12d15690202a89cc
Saravanan M <romansaravanan143@gmail.com>
2026-03-18 15:11:13 +0530

    Delete SampleDataRepository.java

--- SampleDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
- 
-import com.model.SampleData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
- 
-@Repository
-public interface SampleDataRepository extends JpaRepository<SampleData, String> {
-}
